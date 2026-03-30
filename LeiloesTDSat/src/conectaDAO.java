198dee8bdf9dedc5385582ce0d5395bd74ece6ca
Matheus <alasteri32@gmail.com>
2026-03-30 18:55:19 -0300

    Implementado cadastro de produtos no banco

--- LeiloesTDSat/src/conectaDAO.java
+++ LeiloesTDSat/src/conectaDAO.java
@@ -22,11 +22,16 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/uc11?useSSL=false&allowPublicKeyRetrieval=true",
+                "root",
+                "root"
+            );
             
         } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO: " + erro.getMessage());
         }
+        
         return conn;
     }
     
