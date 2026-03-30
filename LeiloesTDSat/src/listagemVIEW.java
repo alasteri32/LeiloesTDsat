4f020e20c3be8bbcd6f40d0c408b08e19232adc8
Matheus <alasteri32@gmail.com>
2026-03-30 19:31:42 -0300

    Corrigida tabela da tela de vendas (Jtable nome errado)

--- LeiloesTDSat/src/listagemVIEW.java
+++ LeiloesTDSat/src/listagemVIEW.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -136,12 +137,15 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+      String id = id_produto_venda.getText();
+
+    ProdutosDAO produtosdao = new ProdutosDAO();
+
+    produtosdao.venderProduto(Integer.parseInt(id));
+
+    JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+    listarProdutos();
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
